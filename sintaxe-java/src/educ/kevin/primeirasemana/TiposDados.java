ffe7f6d56cfad34ac6fbc48ef65f178b873dd49d
Kevyindev <kevinsilvar82@gmail.com>
2024-04-26 13:15:37 -0300

    correção de dados

--- sintaxe-java/src/educ/kevin/primeirasemana/TiposDados.java
+++ sintaxe-java/src/educ/kevin/primeirasemana/TiposDados.java
@@ -2,10 +2,18 @@ package educ.kevin.primeirasemana;
 
 public class TiposDados {
   public static void main(String[] args) {
+
+    //Tipos de Strings para textos
+    String meuNome = "Kevyn Rodrigues";
+    System.out.println("Meu nome completo: " + meuNome);
+
+    //Tipos de dados
     byte idade = 123;
-    short ano = 2021;
+    short ano = 2002;
     int cep = 56200000;
     float pi = 3.14F;
     double salario = 1275.33;
+    System.out.println("Minha idade: " + idade + "\nAno de Nascimento: " + ano + "\nMeu CEP: " + cep + "\nNúmero PI: " + pi + "\nMeu salário: " + salario); //Quebra de linha "\n"
+
   }
 }
