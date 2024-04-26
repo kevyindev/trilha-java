ad363663d04ca0f141046d6786ce19ac096eb938
Kevyindev <kevinsilvar82@gmail.com>
2024-04-26 12:57:44 -0300

    Organizando pastas e adicionando classes

---
+++ sintaxe-java/src/educ/kevin/primeirasemana/TiposDados.java
@@ -0,0 +1,11 @@
+package educ.kevin.primeirasemana;
+
+public class TiposDados {
+  public static void main(String[] args) {
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 56200000;
+    float pi = 3.14F;
+    double salario = 1275.33;
+  }
+}
