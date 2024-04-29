d6486f6610be78b44300a9d0ebcd36588ac1edfb
Kevyindev <kevinsilvar82@gmail.com>
2024-04-29 15:38:46 -0300

    conteúdo sobre operadores finalizado

--- sintaxe-java/src/educ/kevin/primeirasemana/TiposOperadores.java
+++ sintaxe-java/src/educ/kevin/primeirasemana/TiposOperadores.java
@@ -3,14 +3,35 @@ package educ.kevin.primeirasemana;
 public class TiposOperadores {
 
   public static void main(String[] args) {
-    int numero = 5;
-    numero = - numero;
+//   int numero = 5;
+//   numero = - numero;
 
-    System.out.println(numero);
+//   System.out.println(numero);
 
-    numero = numero * -1;
+//   numero = numero * -1;
 
-    System.out.println(numero);
-  }
+//   System.out.println(numero);
+
+//   int numero1 = 1;
+//   int numero2 = 2;
+
+//   boolean simNao = numero1 == numero2;
+
+//   System.out.println("numero Um é igual número Dois? " + simNao);
+
+    boolean condicao1 = false;
+    boolean condicao2 = false;
+
+    if (condicao1 && condicao2) {
+      System.out.println("As duas condições são Verdadeiras!");
+    }
+
+    if (condicao1 || condicao2) {
+      System.out.println("Uma das condições é Verdadeira!");
+    }
+
+    System.out.println("Fim!");
+
+}
 
 }
