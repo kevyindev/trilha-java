5e0b18b22d0c913a77fbad5370774351d99debd9
Kevyindev <kevinsilvar82@gmail.com>
2024-05-02 22:10:21 -0300

    estudos sobre try catch aplicados

--- sintaxe-java/src/educ/kevin/aboutme/SobreMim.java
+++ sintaxe-java/src/educ/kevin/aboutme/SobreMim.java
@@ -1,11 +1,13 @@
 package educ.kevin.aboutme;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class SobreMim {
     public static void main(String[] args) {
         //criando o objeto scanner
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome");
@@ -24,8 +26,12 @@ public class SobreMim {
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
-
+        
         // fechando o Scanner
         scanner.close();
+        }
+        catch (InputMismatchException e) {
+            System.err.println("O campo Idade e Altura precisam ser númericos");
+        }
     }
 }
